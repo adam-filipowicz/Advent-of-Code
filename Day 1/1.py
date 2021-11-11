fa4205898b60438c8eca7af8de3fad55579a815e
afilipowicz <filipowi@ohsu.edu>
2021-11-11 12:48:13 -0800

    Update 1.py

--- Day 1/1.py
+++ Day 1/1.py
@@ -1,4 +1,11 @@
 import itertools
+
+## part 1
 for (a,b) in itertools.combinations([int(n) for n in open('input.txt')], 2):
     if a+b == 2020:
         print(a*b)
+
+## part 2
+for (a,b,c) in itertools.combinations([int(n) for n in open('input.txt')], 3):
+    if a+b+c == 2020:
+        print(a*b*c)
